806e6f01c05b3349b526a6592ec108a1805541bf
victorhundo <victorhundo@gmail.com>
2019-05-28 11:04:36 -0300

    Adicionado form com upload de imagem

--- src/app/pages/feed/new-post/new-post.component.ts
+++ src/app/pages/feed/new-post/new-post.component.ts
@@ -1,7 +1,7 @@
 import { AuthService } from './../../login/auth.service';
 import { FeedService } from './../feed.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Post } from 'src/app/models/post.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -23,29 +23,35 @@ export class NewPostComponent implements OnInit {
               private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.uploadForm = this.formBuilder.group({ titulo: [''], texto: [''], file: [null] })
+    //this.uploadForm = this.formBuilder.group({ titulo: [''], texto: [''], file: [null] })
+    this.uploadForm = new FormGroup({
+      titulo: new FormControl(''),
+      texto: new FormControl(''),
+      theFile: new FormControl(null)
+    })
     this.post.uuidProfessor = this.auth.user.id;
   }
 
   onFileSelect(event) {
-    console.log( Object.getOwnPropertyNames(this.uploadForm.value.titulo))
-    console.log( this.uploadForm.controls['titulo'].value)
+    console.log( this.uploadForm.get('theFile'))
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      this.uploadForm.controls.file.value = file;
+      this.uploadForm.get('theFile').setValue(file);
+      console.log( this.uploadForm.get('theFile').value)
     }
   }
 
   onSubmit(){
 
     const formData = new FormData();
-    formData.append('theFile', this.uploadForm.controls.file.value);
-    formData.append('titulo', 'this.Post.titul');
-    formData.append('texto', 'this.Post.texto');
+    formData.append('theFile', this.uploadForm.get('theFile').value);
+    formData.append('titulo', this.uploadForm.get('titulo').value);
+    formData.append('texto', this.uploadForm.get('texto').value);
 
     this.feddService.submit(formData).subscribe( result =>{
       console.log(result);
       this.postAddFlag = true;
+      alert('Post Publicado com Sucesso');
     });
   }
 }
