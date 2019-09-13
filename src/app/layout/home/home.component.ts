3ffac15cb03ba3e71e0013778345d072c4b4ecb1
victorhundo <victorhundo@gmail.com>
2019-09-13 17:17:53 -0300

    Adicionado novo layout

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   professorFlag = false;
+  opened: boolean;
 
   constructor(private auth: AuthService,
               private route: Router) { }
