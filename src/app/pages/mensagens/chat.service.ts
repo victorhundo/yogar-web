034bc5d8154af628b00997706f339522c410c958
victorhundo <victorhundo@gmail.com>
2019-09-13 10:37:43 -0300

    Adicionado chat privado

--- src/app/pages/mensagens/chat.service.ts
+++ src/app/pages/mensagens/chat.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as io from 'socket.io-client';
 import { API } from './../../API';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
@@ -11,21 +12,30 @@ export class ChatService {
   // private socket: SocketIOClient.Socket;
   private socket: any;
 
-  constructor() {
+  constructor(private http:HttpClient) {
     this.socket = io(`${API}`);
   }
 
   joinRoom(conversation_id: string){
     this.socket.emit('subscribe', conversation_id);
   }
+
   // EMITTER
   sendMessage(msg: any) {
     this.socket.emit('sendMessage', msg);
   }
 
-  getMessage(msgs: string[]){
+  getMessage(msgs: any[]){
     this.socket.on('sendMessage', (data) =>{
       msgs.push(data);
     })
+  }
+
+  getMessageSave(id): Observable<any> {
+	  return this.http.get(`${API}/chat/${id}`);
+  }
+
+  getChatALunos(id): Observable<any> {
+	  return this.http.get(`${API}/chat/${id}/alunos`);
   }
 }
